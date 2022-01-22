7388e15b4f7ce589aed030690c459935aad2df22
root <nagaraja.hm.0001@gmail.com>
2022-01-22 23:42:41 +0100

    202201222341PM - test environment creation

---
+++ tst/tst_Solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "Solution.h"
+
+using namespace std;
+
+int main(
+      int   intc
+   ,  char* intv[]
+){
+   cout<<"Output: "<<Solution(intv[1]);
+
+   cout<<endl;
+   return 0;
+}
+
