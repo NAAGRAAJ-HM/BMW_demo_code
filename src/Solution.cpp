3dabddccf67fb43d188bca8813d7c7690eeeb7ea
root <nagaraja.hm.0001@gmail.com>
2022-01-25 08:43:14 +0100

    202201250842AM - final submission

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -1,33 +1,18 @@
-#include <iostream>
-#include <bitset>
-
-//#include "Solution.h"
-
-using namespace std;
-
-int main(
-      int   intc
-   ,  char* intv[]
-){
-   bitset<32> bit_mask = 1;
-   bitset<32> binary_val(atoi(intv[1]));
-
-   cout<<endl<<"Program: "<<intv[0];
-   cout<<endl<<"intc: "<<intc;
-   cout<<endl<<"intv[1]: "<<intv[1];
-
-   cout<<endl<<"Binary representation: "<<binary_val;
-
+int Solution(int N){
+   int count_max_zeros=0, count_zeros=0, mask=1;
+   bool enable_count=0;
    for(
-      int index_loop = 0;
-          index_loop < 32;
-          index_loop ++
-   ){
-      cout<<endl<<bit_mask;
-      bit_mask = bit_mask << 1;
+      int index_loop=0; index_loop<32; index_loop++){
+      if(N&mask){
+         if(count_zeros>count_max_zeros)count_max_zeros=count_zeros;
+         count_zeros=0;
+         enable_count=1;
+      }
+      else{
+         if(enable_count)count_zeros++;
+      }
+      mask=mask<<1;
    }
-
-   cout<<endl;
-   return 0;
+   return count_max_zeros;
 }
 
